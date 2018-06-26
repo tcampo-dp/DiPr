12a95630a3d10c84d306b37ba4f4b8fe5299b1ae
LAPTOP-MT6T5MHE\Tomas Campo <tcampo@digitalprimeint.com>
2018-06-26 11:03:43 -0500

    Cambios Stack

--- assets/js/cargarStack.js
+++ assets/js/cargarStack.js
@@ -1,30 +1,68 @@
 $(document).ready(function () {
    
-})
+}).on("click", "#tabStack tr", function(e) {
+  var valores;
+  var texTemp ;
+
+  valores = $(this).find('td');
+ 
+  texTemp = $(valores[1]).html();
+
+
+  if (texTemp === localStorage.getItem("textemp")) {
+    $('#descStack').toggle('fast');
+    var InstanseID = $(valores[2]).html();
+    $("#InstanseID").text(InstanseID);
+    console.log($("#InstanseID").text(InstanseID));
+  }else {
+    $('#descStack').show('fast');
+    var InstanseID = $(valores[2]).html();
+    $("#InstanseID").text(InstanseID);
+  }
+  
+  localStorage.setItem("textemp", texTemp);
+
+});
+
+function addStack(Name, Desc) {
+  var date = moment().format('L');
+  var row = 
+    ' <tr>' +
+      '<td class="text-center" style="width: 50px;"><span class="fa fa-desktop text-info"></span></td>' +
+      '<td class="NameServer">'+ Name +'</td>' +
+      '<td>'+ Desc +'</td>' +
+      '<td>0</td>' +
+      '<td>0</td>' +
+      '<td>0</td>' +
+      '<td>'+ date +'</td>' +
+    '</tr>';
+
+  $('#tabStack').append(row);
+
+}
 
 function cargarStack() {
-    $.ajax({
-      type: "GET",
-      url: "stack.html",
-      data: {},
-      success: function (datos) {
-        $('#app').html(datos);
-
-        $('#tabStack').hide();
-        $('#Stack').hide();
-        $('#descStack').hide();
-
-        $('#btnAddStack').click(function () {
-          $('#Stack').show('fast');
-          $('#tabStack').show();
-          $('#ModalStack').modal('toggle');
-        })
-
-        $('#tabStack>tbody>tr').click(function () {
-          $('#descStack').toggle('fast');
-        })
-      }
-    });
+  $.ajax({
+    type: "GET",
+    url: "stack.html",
+    data: {},
+    success: function (datos) {
+      $('#app').html(datos);
+      $('#tabStack').hide();
+      $('#Stack').hide();
+      $('#descStack').hide();
+      $('#btnAddStack').click(function () {
+        $('#Stack').show('fast');
+        $('#tabStack').show();
+        $('#ModalStack').modal('toggle');
+        var Name = $('#txtNameStack').val();
+        var Desc = $('#txtDescStack').val();
+        addStack(Name, Desc);
+        $('#txtNameStack').val('');
+        $('#txtDescStack').val('');
+      })
+    }
+  });
 }
 
 
