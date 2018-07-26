71732920cf3b91f635f966e6a012139e09f06768
LAPTOP-MT6T5MHE\Tomas Campo <tcampo@digitalprimeint.com>
2018-07-26 14:54:51 -0500

    Cambios JS

--- assets/js/cargarServer.js
+++ assets/js/cargarServer.js
@@ -1,125 +1,182 @@
-$(document).ready(function () {
-   
-}).on("click", "#tabServer tr", function(e) {
-  var valores;
-  var texTemp ;
 
-  valores = $(this).find('td');
+ var IdJson;
+ $(document).ready(function () {
+    
+ }).on("click", "#tabServer tr", function(e) {
+   var valores;
+   var texTemp ;
  
-  texTemp = $(valores[1]).html();
-
-
-  if (texTemp === localStorage.getItem("textemp")) {
-    $('#descServer').toggle('fast');
-    $('#text-desc').toggle();
-
-    var InstanceID = $(valores[2]).html();
-    $("#InstanceID").text(InstanceID);
-    console.log($("#InstanceID").text(InstanceID));
-  }else {
-    $('#descServer').show('fast');
-    $('#text-desc').hide();
-
-    var InstanceID = $(valores[2]).html();
-    $("#InstanceID").text(InstanceID);
-  }
+   valores = $(this).find('td');
   
-  localStorage.setItem("textemp", texTemp);
-
-});
-
-function addServer(Name, Desc) {
-  var date = moment().format('L');
-  var row = 
-    ' <tr>' +
-      '<td class="text-center" style="width: 50px;"><span class="fa fa-desktop text-info"></span></td>' +
-      '<td class="NameServer">'+ Name +'</td>' +
-      '<td>'+ Desc +'</td>' +
-      '<td>0</td>' +
-      '<td>0</td>' +
-      '<td>0</td>' +
-      '<td>'+ date +'</td>' +
-    '</tr>';
-
-    
-    // var jsonData = {};
-    // jsonData.Server = {"NameServer" : Name, "descServer" : Desc, "Date": date}
-    // localStorage.setItem("ServerJ", JSON.stringify(jsonData));
-
-    var data = [];
-
-    if (localStorage.getItem("ServerJ") === null) {
-      data.push({'NameServer' : Name, 'DescServer' : Desc, 'Date': date});
-    }else{
-      var _jsonData ={ _Stack:[]};
-      _jsonData._Stack  = JSON.parse(localStorage.getItem('ServerJ'));
-      $.each(_jsonData._Stack, function (index, item) { 
-         data.push(item);
-      });
-      data.push({'NameServer' : Name, 'DescServer' : Desc, 'Date': date});
-    }
-
-    localStorage.setItem("ServerJ", JSON.stringify(data));
-
-
-    
-
-  $('#tabServer').append(row);
-
-}
-
-function cargarServer() {
-  $.ajax({
-    type: "GET",
-    url: "Server.html",
-    data: {},
-    success: function (datos) {
-      $('#app').html(datos);
-
-      $('#descServer').hide();
-
-      if (localStorage.getItem("ServerJ") !== null) {
-       var _ServerJ = $.parseJSON(localStorage.getItem("ServerJ"));
-        console.log(_ServerJ);
-        $.each(_ServerJ, function (i, item) { 
-           var tr =  
-          ' <tr>' +
-              '<td class="text-center" style="width: 50px;"><span class="fa fa-desktop text-info"></span></td>' +
-              '<td class="NameServer">'+ item.NameServer +'</td>' +
-              '<td>'+ item.DescServer +'</td>' +
-              '<td>0</td>' +
-              '<td>0</td>' +
-              '<td>0</td>' +
-              '<td>'+ item.Date +'</td>' +
-          '</tr>';
-
-         
-
-          $('#tabServer').append(tr);
-        });
-      } else {
-        console.log("Vacio Server")
+   texTemp = $(valores[2]).html();
+ 
+ 
+   if (texTemp === localStorage.getItem("textemp")) {
+     $('#descServer').toggle('fast');
+     $('#text-desc').toggle();
+ 
+     var InstanceID = $(valores[3]).html();
+     $("#InstanceID").text(InstanceID);
+   }else {
+     $('#descServer').show('fast');
+     $('#text-desc').hide();
+ 
+     var InstanceID = $(valores[2]).html();
+     $("#InstanceID").text(InstanceID);
+   }
+   
+   localStorage.setItem("textemp", texTemp);
+ 
+ }).on("click", "#btnActionServer", function () {
+   $('#divActionServer').toggle();
+ }).on("click", "#btnUpdateServer", function () {
+    IdJson = $('table input:checked').map(function(){
+    return $(this).closest('tr').find('td:eq(8)').text();
+    }).get();
+ 
+    jsonValue = JSON.parse(localStorage.getItem('ServerJ'));
+    if (IdJson.length <= 1) {
+      for (let i = 0; i < jsonValue.length ; i++) {
+        if (jsonValue[i].id == IdJson) {
+          InfoServer = jsonValue[i];
+        }
       }
-      var response = $.parseJSON(localStorage.getItem("StackJ"));
-      console.log(response);
-      $.each(response, function (i, item) { 
-        $("#selectStack").append('<option value="'+ item.NameStack +'">'+ item.NameStack +'</option>');
-      });
-
-      $('#btnAddServer').click(function () {
-        $('#Server').show('fast');
-        $('#tabServer').show();
-        $('#ModalServer').modal('toggle');
-        var Name = $('#txtNameServer').val();
-        var Desc = $('#txtDescServer').val();
-
-        addServer(Name, Desc);
-        $('#txtNameServer').val('');
-        $('#txtDescServer').val('');
-      })
+      $('#txtNameServer').val(InfoServer.NameServer);
+      $('#txtDescServer').val(InfoServer.DescServer);
     }
-  });
-}
+    
+   
+ 
+     
+ }).on("click", "#btnDeleteServer", function () {
+   IdJson = $('table input:checked').map(function(){
+     return $(this).closest('tr').find('td:eq(8)').text();
+     }).get();
+  
+     jsonValue = JSON.parse(localStorage.getItem('ServerJ'));
+  
+  
+    for (let i = 0; i < jsonValue.length ; i++) {
+      if (jsonValue[i].id == IdJson) {
+        InfoServer = jsonValue[i];
+         jsonValue.splice(i, 1);
+ 
+      }
+    }
+ 
+     
+     localStorage.setItem("ServerJ", JSON.stringify(jsonValue));
+     
+     $('input:checkbox:checked').map(function () {
+       return this.value;
+     }).get();
+     $('input:checkbox:checked').parents("tr").remove();
+ });
+ 
+ 
+ var id;
+ 
+ function addServer(Name, Desc) {
+   var date = moment().format('L');
+   
+   var data = [];
+   
+   
+   if (localStorage.getItem("ServerJ") === null) {
+     id = 1;
+     data.push({'id': id, 'NameServer' : Name, 'DescServer' : Desc, 'Date': date});
+   }else{
+     var _jsonData ={ _Server:[]};
+     _jsonData._Server  = JSON.parse(localStorage.getItem('ServerJ'));
+     if (_jsonData._Server.length === 0) {
+       id = 1;
+       
+     } else {
+       $.each(_jsonData._Server, function (index, item) { 
+         data.push(item);
+         id = item.id + 1;
+       });
+       
+     }
+     data.push({'id': id, 'NameServer' : Name, 'DescServer' : Desc, 'Date': date});
+   }
+   var row = 
+     ' <tr>' +
+       '<td style="width: 40px;text-align: center;"><input type="checkbox" id="chkServer"></td>' +
+       '<td class="text-center" style="width: 50px;"><span class="fa fa-desktop text-info"></span></td>' +
+       '<td class="NameServer">'+ Name +'</td>' +
+       '<td>'+ Desc +'</td>' +
+       '<td>0</td>' +
+       '<td>0</td>' +
+       '<td>0</td>' +
+       '<td>'+ date +'</td>' +
+       '<td style="display: none;" id="idServer">'+ id +'</td>' +
+     '</tr>';
+ 
+     localStorage.setItem("ServerJ", JSON.stringify(data));
+   
+   $('#tabServer').append(row);
+ 
+ }
+ 
+ function cargarServer() {
+   $.ajax({
+     type: "GET",
+     url: "Server.html",
+     data: {},
+     success: function (datos) {
+       $('#app').html(datos);
+ 
+       $('#descServer').hide();
+       $('#divActionServer').hide();
+ 
+       if (localStorage.getItem("ServerJ") !== null) {
+         response = $.parseJSON(localStorage.getItem("ServerJ"));
+         $.each(response, function (i, item) { 
+            var tr =  
+           ' <tr>' +
+               '<td style="width: 40px;text-align: center;"><input type="checkbox" id="chkServer"></td>' +
+               '<td class="text-center" style="width: 50px;"><span class="fa fa-desktop text-info"></span></td>' +
+               '<td class="NameServer">'+ item.NameServer +'</td>' +
+               '<td>'+ item.DescServer +'</td>' +
+               '<td>0</td>' +
+               '<td>0</td>' +
+               '<td>0</td>' +
+               '<td>'+ item.Date +'</td>' +
+               '<td style="display: none;" id="idServer">'+ item.id +'</td>' +
+           '</tr>';
+           $('#tabServer').append(tr);
+         });
+       }
 
-
-
+       var response = $.parseJSON(localStorage.getItem("StackJ"));
+       console.log(response);
+       $.each(response, function (i, item) { 
+         $("#selectStack").append('<option value="'+ item.NameStack +'">'+ item.NameStack +'</option>');
+       });
+ 
+       $('#btnAddServer').click(function () {
+         $('#Server').show('fast');
+         $('#tabServer').show();
+         $('#ModalServer').modal('toggle');
+         var Name = $('#txtNameServer').val();
+         var Desc = $('#txtDescServer').val();
+ 
+         
+ 
+         addServer(Name, Desc);
+         $('#txtNameServer').val('');
+         $('#txtDescServer').val('');
+       })
+ 
+       $('#btnCloseModalServer').click(function (e) { 
+         $('#txtNameServer').val('');
+         $('#txtDescServer').val('');
+       });
+     }
+   });
+ }
+ 
+ 
+ 
+ 
--- assets/js/cargarWorkSpace.js
+++ assets/js/cargarWorkSpace.js
@@ -1,84 +1,141 @@
-$(document).ready(function () {
-   
-}).on("click", "#tabWorkSpaces tr", function(e) {
-  var valores;
-  var texTemp ;
 
-  valores = $(this).find('td');
+ var IdJson;
+ $(document).ready(function () {
+    
+ }).on("click", "#tabWorkSpaces tr", function(e) {
+   var valores;
+   var texTemp ;
  
-  texTemp = $(valores[1]).html();
-
-
-  if (texTemp === localStorage.getItem("textemp")) {
-    $('#descWorkSpaces').toggle('fast');
-    $('#text-desc').toggle();
-
-    var InstanceID = $(valores[2]).html();
-    $("#InstanceID").text(InstanceID);
-    console.log($("#InstanceID").text(InstanceID));
-  }else {
-    $('#descWorkSpaces').show('fast');
-    $('#text-desc').hide();
-
-    var InstanceID = $(valores[2]).html();
-    $("#InstanceID").text(InstanceID);
-  }
+   valores = $(this).find('td');
   
-  localStorage.setItem("textemp", texTemp);
-
-});
-
-var jsonData = {};
-
-
-function addWorkSpaces(Name, Desc) {
-  var date = moment().format('L');
-  var row = 
-    ' <tr>' +
-      '<td class="text-center" style="width: 50px;"><span class="fa fa-desktop text-info"></span></td>' +
-      '<td class="NameWorkSpaces">'+ Name +'</td>' +
-      '<td>'+ Desc +'</td>' +
-      '<td>0</td>' +
-      '<td>0</td>' +
-      '<td>0</td>' +
-      '<td>'+ date +'</td>' +
-    '</tr>';
-
-    var data = [];
-
-    if (localStorage.getItem("WorkSpacesJ") === null) {
-      data.push({'NameWorkSpaces' : Name, 'DescWorkSpaces' : Desc, 'Date': date});
-    }else{
-      var _jsonData ={ _WorkSpaces:[]};
-      _jsonData._WorkSpaces  = JSON.parse(localStorage.getItem('WorkSpacesJ'));
-      $.each(_jsonData._WorkSpaces, function (index, item) { 
-         data.push(item);
-      });
-      data.push({'NameWorkSpaces' : Name, 'DescWorkSpaces' : Desc, 'Date': date});
+   texTemp = $(valores[2]).html();
+ 
+ 
+   if (texTemp === localStorage.getItem("textemp")) {
+     $('#descWorkSpaces').toggle('fast');
+     $('#text-desc').toggle();
+ 
+     var InstanceID = $(valores[3]).html();
+     $("#InstanceID").text(InstanceID);
+   }else {
+     $('#descWorkSpaces').show('fast');
+     $('#text-desc').hide();
+ 
+     var InstanceID = $(valores[2]).html();
+     $("#InstanceID").text(InstanceID);
+   }
+   
+   localStorage.setItem("textemp", texTemp);
+ 
+ }).on("click", "#btnActionWorkSpaces", function () {
+   $('#divActionWorkSpaces').toggle();
+ }).on("click", "#btnUpdateWorkSpaces", function () {
+    IdJson = $('table input:checked').map(function(){
+    return $(this).closest('tr').find('td:eq(8)').text();
+    }).get();
+ 
+    jsonValue = JSON.parse(localStorage.getItem('WorkSpacesJ'));
+    if (IdJson.length <= 1) {
+      for (let i = 0; i < jsonValue.length ; i++) {
+        if (jsonValue[i].id == IdJson) {
+          InfoWorkSpaces = jsonValue[i];
+        }
+      }
+      $('#txtNameWorkSpaces').val(InfoWorkSpaces.NameWorkSpaces);
+      $('#txtDescWorkSpaces').val(InfoWorkSpaces.DescWorkSpaces);
     }
-
-    localStorage.setItem("WorkSpacesJ", JSON.stringify(data));
-
-  $('#tabWorkSpaces').append(row);
-
-}
-
-
-function cargarWorkSpaces() {
-  $.ajax({
-    type: "GET",
-    url: "WorkSpaces.html",
-    data: {},
-    success: function (datos) {
-      $('#app').html(datos);
-
-      $('#descWorkSpaces').hide();
-
-      if (localStorage.getItem("WorkSpacesJ") !== null) {
-        var _WorkSpacesJ = $.parseJSON(localStorage.getItem("WorkSpacesJ"));
-         $.each(_WorkSpacesJ, function (i, item) { 
+    
+   
+ 
+     
+ }).on("click", "#btnDeleteWorkSpaces", function () {
+   IdJson = $('table input:checked').map(function(){
+     return $(this).closest('tr').find('td:eq(8)').text();
+     }).get();
+  
+     jsonValue = JSON.parse(localStorage.getItem('WorkSpacesJ'));
+  
+  
+    for (let i = 0; i < jsonValue.length ; i++) {
+      if (jsonValue[i].id == IdJson) {
+        InfoWorkSpaces = jsonValue[i];
+         jsonValue.splice(i, 1);
+ 
+      }
+    }
+ 
+     
+     localStorage.setItem("WorkSpacesJ", JSON.stringify(jsonValue));
+     
+     $('input:checkbox:checked').map(function () {
+       return this.value;
+     }).get();
+     $('input:checkbox:checked').parents("tr").remove();
+ });
+ 
+ 
+ var id;
+ 
+ function addWorkSpaces(Name, Desc) {
+   var date = moment().format('L');
+   
+   var data = [];
+   
+   
+   if (localStorage.getItem("WorkSpacesJ") === null) {
+     id = 1;
+     data.push({'id': id, 'NameWorkSpaces' : Name, 'DescWorkSpaces' : Desc, 'Date': date});
+   }else{
+     var _jsonData ={ _WorkSpaces:[]};
+     _jsonData._WorkSpaces  = JSON.parse(localStorage.getItem('WorkSpacesJ'));
+     if (_jsonData._WorkSpaces.length === 0) {
+       id = 1;
+       
+     } else {
+       $.each(_jsonData._WorkSpaces, function (index, item) { 
+         data.push(item);
+         id = item.id + 1;
+       });
+       
+     }
+     data.push({'id': id, 'NameWorkSpaces' : Name, 'DescWorkSpaces' : Desc, 'Date': date});
+   }
+   var row = 
+     ' <tr>' +
+       '<td style="width: 40px;text-align: center;"><input type="checkbox" id="chkWorkSpaces"></td>' +
+       '<td class="text-center" style="width: 50px;"><span class="fa fa-desktop text-info"></span></td>' +
+       '<td class="NameServer">'+ Name +'</td>' +
+       '<td>'+ Desc +'</td>' +
+       '<td>0</td>' +
+       '<td>0</td>' +
+       '<td>0</td>' +
+       '<td>'+ date +'</td>' +
+       '<td style="display: none;" id="idWorkSpaces">'+ id +'</td>' +
+     '</tr>';
+ 
+     localStorage.setItem("WorkSpacesJ", JSON.stringify(data));
+   
+   $('#tabWorkSpaces').append(row);
+ 
+ }
+ 
+ function cargarWorkSpaces() {
+   $.ajax({
+     type: "GET",
+     url: "WorkSpaces.html",
+     data: {},
+     success: function (datos) {
+       $('#app').html(datos);
+ 
+       $('#descWorkSpaces').hide();
+       $('#divActionWorkSpaces').hide();
+ 
+       if (localStorage.getItem("WorkSpacesJ") !== null) {
+         response = $.parseJSON(localStorage.getItem("WorkSpacesJ"));
+         $.each(response, function (i, item) { 
             var tr =  
            ' <tr>' +
+               '<td style="width: 40px;text-align: center;"><input type="checkbox" id="chkWorkSpaces"></td>' +
                '<td class="text-center" style="width: 50px;"><span class="fa fa-desktop text-info"></span></td>' +
                '<td class="NameServer">'+ item.NameWorkSpaces +'</td>' +
                '<td>'+ item.DescWorkSpaces +'</td>' +
@@ -86,47 +143,51 @@ function cargarWorkSpaces() {
                '<td>0</td>' +
                '<td>0</td>' +
                '<td>'+ item.Date +'</td>' +
+               '<td style="display: none;" id="idWorkSpaces">'+ item.id +'</td>' +
            '</tr>';
            $('#tabWorkSpaces').append(tr);
          });
-       } else {
-         console.log("Vacio WorkSpaces")
        }
 
        if (localStorage.getItem("StackJ") !== null) {
-         var responseStack = $.parseJSON(localStorage.getItem("StackJ"));
-         console.log(responseStack);
-         $.each(responseStack, function (i, item) { 
-           $("#selectStack").append('<option value="'+ item.NameStack +'">'+ item.NameStack +'</option>');
-         });
-       }
+        var responseStack = $.parseJSON(localStorage.getItem("StackJ"));
+        console.log(responseStack);
+        $.each(responseStack, function (i, item) { 
+          $("#selectStack").append('<option value="'+ item.NameStack +'">'+ item.NameStack +'</option>');
+        });
+      }
 
-       if (localStorage.getItem("ServerJ") !== null) {
-         var responseServer = $.parseJSON(localStorage.getItem("ServerJ"));
-         console.log(responseServer);
-         $.each(responseServer, function (i, item) { 
-           $("#selectServer").append('<option value="'+ item.NameServer +'">'+ item.NameServer +'</option>');
-         });
-       }
-
-
-      $('#btnAddWorkSpaces').click(function () {
-        $('#WorkSpaces').show('fast');
-        $('#tabWorkSpaces').show();
-        $('#ModalWorkSpaces').modal('toggle');
-        var Name = $('#txtNameWorkSpaces').val();
-        var Desc = $('#txtDescWorkSpaces').val();
-
-        
-       
-
-        addWorkSpaces(Name, Desc);
-        $('#txtNameWorkSpaces').val('');
-        $('#txtDescWorkSpaces').val('');
-      })
-    }
-  });
-}
-
-
-
+      if (localStorage.getItem("ServerJ") !== null) {
+        var responseServer = $.parseJSON(localStorage.getItem("ServerJ"));
+        console.log(responseServer);
+        $.each(responseServer, function (i, item) { 
+          $("#selectServer").append('<option value="'+ item.NameServer +'">'+ item.NameServer +'</option>');
+        });
+      }
+   
+ 
+       $('#btnAddWorkSpaces').click(function () {
+         $('#WorkSpaces').show('fast');
+         $('#tabWorkSpaces').show();
+         $('#ModalWorkSpaces').modal('toggle');
+         var Name = $('#txtNameWorkSpaces').val();
+         var Desc = $('#txtDescWorkSpaces').val();
+ 
+         
+ 
+         addWorkSpaces(Name, Desc);
+         $('#txtNameWorkSpaces').val('');
+         $('#txtDescWorkSpaces').val('');
+       })
+ 
+       $('#btnCloseModalWorkSpaces').click(function (e) { 
+         $('#txtNameWorkSpaces').val('');
+         $('#txtDescWorkSpaces').val('');
+       });
+     }
+   });
+ }
+ 
+ 
+ 
+ 
